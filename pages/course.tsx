3126dd53063a925b504dc03e773dcfbe099137e5
m-abdelwahab <hey@mahmoud.codes>
2021-10-25 21:14:26 +0200

    add landing page for the course

---
+++ pages/course.tsx
@@ -0,0 +1,12 @@
+import { Pricing } from 'components/course/Pricing';
+import React from 'react';
+
+const Course = () => {
+  return (
+    <div>
+      <Pricing />
+    </div>
+  );
+};
+
+export default Course;
