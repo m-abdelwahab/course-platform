18c011d348fb5ed014825adeef3610dcb79389f3
m-abdelwahab <hey@mahmoud.codes>
2021-10-25 21:24:43 +0200

    add page for email confirmation when a user logs in

---
+++ pages/check-your-email.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CheckYourEmail = () => {
+  return (
+    <div className="prose mx-auto my-16">
+      <h1>Check your email!</h1>
+
+      <p>
+        A login link has been sent to your email! Use it and you&apos;ll be able
+        to access the content 🚀
+      </p>
+    </div>
+  );
+};
+
+export default CheckYourEmail;
